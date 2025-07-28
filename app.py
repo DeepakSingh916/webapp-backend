0bb8876400210e5bc28a73ce438f7a257bd866eb
Deepak <ds248776@gmail.com>
2025-07-28 23:15:53 +0530

    msg changed

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def login():
 
 @app.route("/home", methods=["GET"])
 def home():
-    return jsonify({"message": "Welcome to web hello rani"})
+    return jsonify({"message": "Welcome to web hello rani deepak"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
